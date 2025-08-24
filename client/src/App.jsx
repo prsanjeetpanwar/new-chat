b9a29034ef55e750b260b9a086750259753140fb
Prasanjeet Panwar <ppanwar@cholachariots.com>
2025-08-24 17:23:01 +0530

    completed the intial frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Route, Router , Routes} from 'react-router'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import NotificationPage from './pages/NotificationPage'
+import ChatPage from './pages/ChatPage'
+import OnBoardingPage from './pages/OnBoardingPage'
+import CallPage from './pages/CallPage'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen' data-theme="synthwave" >
+      <h2>hello</h2>
+       <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/sign-up' element={<SignUpPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/Notification' element={<NotificationPage/>}/>
+        <Route path='/chat' element={<ChatPage/>}/>
+        <Route path='/onBoarding' element={<OnBoardingPage/>}/>
+      <Route path='/call' element={<CallPage/>}/>
+
+       </Routes>
+    </div>
   )
 }
 
---
+++ client/src/pages/OnBoardingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OnBoardingPage = () => {
+  return (
+    <div>
+      onBoarding Page
+    </div>
+  )
+}
+
+export default OnBoardingPage
